7e38e111a571bb43c925637c799a8cb9cb483f0e
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-04-13 15:31:48 +0300

    ccd4thd1

---
+++ story_content/user.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6GdynQTmM1e":
+        Script1();
+        break;
+      case "5oxDJxpqkck":
+        Script2();
+        break;
+      case "6EzjTnqGOYF":
+        Script3();
+        break;
+      case "5aHEUV3wzTD":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  window.print();
+}
+
